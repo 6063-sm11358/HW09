cf1cf963b230963f1f922c2db673b9b87bac8356
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-11-06 17:51:37 -0500

    Code v1.0

--- sketch.js
+++ sketch.js
@@ -1,6 +1,53 @@
-function setup() {
+let speechOut;
+let speechRec;
+let continuous;
+let interimResults;
+
+let xPos;
+let yPos;
+let rectSize;
+
+function setup()
+{
   createCanvas(windowWidth, windowHeight);
+  speechOut = new p5.Speech();
+  
+  continuous = true;
+  interimResults = true;
+  speechRec = new p5.SpeechRec(speechFunc);
+  speechRec.start(continuous,interimResults);
+
+  xPos = width/2;
+  yPos = height/2;
+  rectSize = 30;
 }
 
-function draw() {
+function speechFunc(speech)
+{
+  if(speech.text.includes('left'))
+  {
+    speechOut.speak('Okay. Moving to the left');
+    xPos-=30;
+  }
+  else if(speech.text.includes('right'))
+  {
+    speechOut.speak('Okay. Moving to the right');
+    xPos+=30;
+  }
+  
+  print(speech.text);
+}
+
+function draw()
+{
+  background(255);
+  
+  fill('red');
+  noStroke();
+  rect(xPos, yPos, rectSize, rectSize);
+}
+
+function mouseClicked()
+{
+  speechOut.speak('Hello there');
 }
